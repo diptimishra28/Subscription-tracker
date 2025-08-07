0ea2d48c84a562f17c443296dcefcf1e0451d6b9
diptimishra28 <diptimishra046@gmail.com>
2025-08-07 09:02:30 +0530

    Implemented model logic and middlewarehandler

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import { PORT } from './config/env.js';
-
+                                              
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/db_connection.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
+
+app.use(express.json()); //this allows app to handle json data sent in requests or api calls
+app.use(express.urlencoded({extends: false})); //this helps to process the form data sent via html forms in a  simple format
+app.use(cookieParser);
 
 //api/v1/auth/sign-up
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+
+app.use(errorMiddleware)
 
 app.get('/', (req, res) => {
     res.send('welcome to the Subscription Tracker API!');
---
+++ models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'User Name is required'],
+        trim: true,
+        minLength: 3,
+        maxLength:50,
+    },
+    email: {
+        type: String,
+        required: [true, 'User Email is required'],
+        unique: true,
+        lowercase: true,
+        match: [ /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please enter a valid email address' ] //regex
+
+    },
+    password: {
+        type: String,
+        required: [true, 'user password is required'],
+        minLength: 6,
+    },
+    age: {
+        type: Number,
+        required: [true, 'Age is required'],
+        min: [16, 'Age must be at least 16'],
+        max: [100, 'Age must be less than or equal to 100']
+    }
+
+}, {timestamps: true}); //create,update timing save in db if put true
+
+const User = mongoose.model('User', userSchema); //making usertable
+
+export default User;
