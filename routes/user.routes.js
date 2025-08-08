7115e40160b7dcb473618d6aca52ed6ae5613586
diptimishra28 <diptimishra046@gmail.com>
2025-08-09 02:49:00 +0530

    Added JWT authentication and authorization middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 
+import authorize from '../middlewares/auth.middleware.js'
+import {getUser, getUsers} from '../controllers/user.controller.js'
+
 const userRouter = Router();
 
 //GET /users -> get all users
 //GET /users/:id -> get user by id // 23 4123 1234
 
-userRouter.get('/', (req, res) => res.send({title: 'GET all users'}));
+//userRouter.get('/', (req, res) => res.send({title: 'GET all users'}));
+userRouter.get('/', getUsers);
 
-userRouter.get('/:id', (req, res) => res.send({title: 'GET users details'}));
+userRouter.get('/:id', authorize, getUsers);
 
 userRouter.post('/', (req, res) => res.send({title: 'CREATE new users'}));
 
